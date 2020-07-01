69efed912a87a1c473c646d57fda8da059cc551c
Deputatov Nikita <deputatovn@yandex.ru>
2020-07-01 17:53:51 +0300

    add get solution

--- __test__/arrays.test.js
+++ __test__/arrays.test.js
@@ -1,10 +1,32 @@
-import { swap } from '../src/arrays.js';
+import { swap, get } from '../src/arrays.js';
 
-test('array', () => {
+test('swap', () => {
   expect(swap([])).toEqual([]);
   expect(swap([1])).toEqual([1]);
   expect(swap([1, 2])).toEqual([2, 1]);
   expect(swap(['one', 'two', 'three'])).toEqual(['three', 'two', 'one']);
   expect(swap(['one', 'two', 'three', 'four'])).toEqual(['four', 'two', 'three', 'one']);
   expect(swap(['one', 'two', 'three', 'four', ''])).toEqual(['', 'two', 'three', 'four', 'one']);
+});
+
+test('get', () => {
+  const cities = ['moscow', 'london', 'berlin', 'porto', ''];
+
+  const actual1 = get(cities, 0);
+  expect(actual1).toBe(cities[0]);
+
+  const actual2 = get(cities, 2, 'default');
+  expect(actual2).toBe(cities[2]);
+
+  const actual3 = get(cities, 5, false);
+  expect(actual3).toBe(false);
+
+  const actual4 = get(cities, -1, 'oops');
+  expect(actual4).toBe('oops');
+
+  const actual5 = get(cities, 5);
+  expect(actual5).toBeNull();
+
+  const actual6 = get(cities, 4);
+  expect(actual6).toBe('');
 });
--- src/arrays.js
+++ src/arrays.js
@@ -2,3 +2,10 @@ export const swap = (arr) => {
   if (arr.length < 2) return arr;
   return [arr[arr.length - 1], ...arr.slice(1, arr.length - 1), arr[0]];
 };
+
+export const get = (arr, idx, def = null) => {
+  if (arr[idx] === undefined) {
+    return def;
+  }
+  return arr[idx];
+};
