c18a7f691da4ed1e840d2eef642615211e414d05
Deputatov Nikita <deputatovn@yandex.ru>
2020-07-03 18:37:26 +0300

    add get super series winner solution

---
+++ src/superseries.js
@@ -0,0 +1,9 @@
+export const getSuperSeriesWinner = (scores) => {
+  const result = scores.reduce((acc, [can, ussr]) => {
+    const res = Math.sign(can - ussr);
+    return acc + res;
+  }, 0);
+  if (result > 0) { return 'canada'; }
+  if (result < 0) { return 'ussr'; }
+  return null;
+};
