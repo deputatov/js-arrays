5d83c8415080c20ffc6059d1b623486705939f37
Deputatov Nikita <deputatovn@yandex.ru>
2020-07-07 13:35:56 +0300

    add bracket structure balanced solution

--- __test__/strings.test.js
+++ __test__/strings.test.js
@@ -1,4 +1,6 @@
-import { buildDefinitionList, makeCensored, countUniqChars } from '../src/strings.js';
+import {
+  buildDefinitionList, makeCensored, countUniqChars, isBracketStructureBalanced
+} from '../src/strings.js';
 
 it('StringsTest', () => {
   const definitions1 = [
@@ -53,4 +55,46 @@ it('countUniqChars', () => {
   const text4 = '0';
   const actual4 = countUniqChars(text4);
   expect(actual4).toBe(1);
+});
+
+describe('is bracket structure balanced', () => {
+  test('should be balanced', () => {
+    const str = '()';
+    expect(isBracketStructureBalanced(str)).toBe(true);
+
+    const str2 = '[()]';
+    expect(isBracketStructureBalanced(str2)).toBe(true);
+
+    const str3 = '({}[])';
+    expect(isBracketStructureBalanced(str3)).toBe(true);
+
+    const str4 = '(<><<{[()]}>>)';
+    expect(isBracketStructureBalanced(str4)).toBe(true);
+
+    const str5 = '';
+    expect(isBracketStructureBalanced(str5)).toBe(true);
+  });
+
+  test('should not be balanced', () => {
+    const str1 = '((';
+    expect(isBracketStructureBalanced(str1)).toBe(false);
+
+    const str2 = '[[()]';
+    expect(isBracketStructureBalanced(str2)).toBe(false);
+
+    const str3 = '({}}[]';
+    expect(isBracketStructureBalanced(str3)).toBe(false);
+
+    const str4 = '(<><<{[()]}>>>)';
+    expect(isBracketStructureBalanced(str4)).toBe(false);
+
+    const str5 = '}';
+    expect(isBracketStructureBalanced(str5)).toBe(false);
+
+    const str6 = '([)]';
+    expect(isBracketStructureBalanced(str6)).toBe(false);
+
+    const str7 = '([))';
+    expect(isBracketStructureBalanced(str7)).toBe(false);
+  });
 });
--- src/strings.js
+++ src/strings.js
@@ -1,3 +1,6 @@
+// @ts-check
+/* eslint no-restricted-syntax: ["off", "ForOfStatement"] */
+
 export const buildDefinitionList = (definitions) => {
   if (definitions.length === 0) {
     return '';
@@ -16,3 +19,25 @@ export const makeCensored = (sentence, censors) => sentence
   .join(' ');
 
 export const countUniqChars = (text) => new Set(text.split('')).size;
+
+const openingSymbols = ['(', '[', '{', '<'];
+const closingSymbols = [')', ']', '}', '>'];
+
+const isOpeningSymbol = (symbol) => openingSymbols.includes(symbol);
+const getClosingSymbolFor = (symbol) => closingSymbols[openingSymbols.indexOf(symbol)];
+
+export const isBracketStructureBalanced = (str) => {
+  const stack = [];
+  for (const symbol of str) {
+    if (isOpeningSymbol(symbol)) {
+      const closingSymbol = getClosingSymbolFor(symbol);
+      stack.push(closingSymbol);
+    } else {
+      const lastSavedSymbol = stack.pop();
+      if (symbol !== lastSavedSymbol) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+};
