a6f5112309233150cc7eeca4532ab6dcc673fc26
Nikita Deputatov <deputatovn@gmail.com>
2021-03-26 14:27:16 +0300

    add more solutions for task

--- __test__/getWeekends.test.js
+++ __test__/getWeekends.test.js
@@ -1,9 +1,20 @@
-import { getWeekends } from '../src/getWeekends';
+import { expect } from '@jest/globals';
+import { getWeekends, getWeekends1, getWeekends2 } from '../src/getWeekends';
 
 describe('get weekends', () => {
-  test('should word', () => {
+  test('should work1', () => {
     expect(getWeekends()).toEqual(['saturday', 'sunday']);
     expect(getWeekends('long')).toEqual(['saturday', 'sunday']);
     expect(getWeekends('short')).toEqual(['sat', 'sun']);
+  });
+  test('should work2', () => {
+    expect(getWeekends1()).toEqual(['saturday', 'sunday']);
+    expect(getWeekends1('long')).toEqual(['saturday', 'sunday']);
+    expect(getWeekends1('short')).toEqual(['sat', 'sun']);
+  });
+  test('should work3', () => {
+    expect(getWeekends2()).toEqual(['saturday', 'sunday']);
+    expect(getWeekends2('long')).toEqual(['saturday', 'sunday']);
+    expect(getWeekends2('short')).toEqual(['sat', 'sun']);
   });
 });
--- src/getWeekends.js
+++ src/getWeekends.js
@@ -4,3 +4,24 @@ const weekends = ['saturday', 'sunday'];
 
 export const getWeekends = (format = 'long') =>
   format === 'short' ? weekends.map((value) => value.slice(0, 3)) : weekends;
+
+export const getWeekends1 = (format = 'long') => {
+  const short = ['sat', 'sun'];
+  const long = ['saturday', 'sunday'];
+  switch (format) {
+    case 'long':
+      return long;
+    case 'short':
+      return short;
+    default:
+      return long;
+  }
+};
+
+export const getWeekends2 = (format = 'long') => {
+  const weekends1 = {
+    short: ['sat', 'sun'],
+    long: ['saturday', 'sunday'],
+  };
+  return weekends1[format] ?? weekends1.long;
+};
